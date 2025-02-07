6ef85f7a44dad22a19e492ca360cf3172c72f003
aklema094 <aklemasultana094@gmail.com>
2025-02-07 19:48:25 +0600

    fatch api with url

--- Fatch api/app.js
+++ Fatch api/app.js
@@ -1,11 +1,15 @@
 let url = "https://api.thecatapi.com/v1/images/0XYvRd7oD";
+let image = document.querySelector("#image");
 
 let respose = fetch(url);
 console.log(respose);
 
 const getData = async () =>{
     let res = await fetch(url);
-    console.log(res);
+    console.log(res);// response in json format
     console.log(res.status);
+    let data = await res.json();
+    console.log(data);
+    image.src = data.url;
 }
 console.log(getData());
