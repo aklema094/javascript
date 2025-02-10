e2c8c85d3eeaf0f9150f39efc1001ab737f0ff1b
aklema094 <aklemasultana094@gmail.com>
2025-02-10 23:11:41 +0600

    solved 17

--- 50 problem challange/app.js
+++ 50 problem challange/app.js
@@ -113,5 +113,19 @@ let isPrime = (number) =>{
 }
 isPrime(3);
 
+// calcute the sum of digits of a positive integer number
+
+function sumDigits(n){
+    let s = n.toString();
+    let sum = 0;
+    for(let char of s){
+        let digit = parseInt(char);
+        sum+= digit;
+    }
+    console.log("Sum is : ",sum);
+}
+
+sumDigits(123);
+
 
 
